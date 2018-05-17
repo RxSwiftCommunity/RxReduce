06b221292fe81c8db5654a9a42cfc566cffdb492
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2018-05-17 13:37:01 -0400

    Add middleware integration

--- RxReduce/Store.swift
+++ RxReduce/Store.swift
@@ -13,6 +13,9 @@ import RxCocoa
 /// A Reducer mutates an input state into an output state according to an action
 public typealias Reducer<StateType: State> = (_ state: StateType?, _ action: Action) -> StateType
 
+/// A Middleware has not effect on the state, it us just triggered by a dispatch action
+public typealias Middleware<StateType: State> = (_ state: StateType?, _ action: Action) -> Void
+
 /// A Store holds the state, mutate the state through actions / reducers and exposes the state via a Driver
 /// A Store is dedicated to a State Type
 public protocol Store {
@@ -26,7 +29,8 @@ public protocol Store {
     /// Inits the Store with its reducers stack
     ///
     /// - Parameter reducers: the reducers to be executed by the dispatch function
-    init(withReducers reducers: [Reducer<StateType>])
+	/// - Parameter middlewares: the middlewartes to be executed by the dispatch function (the will be executed in reverse order)
+	init(withReducers reducers: [Reducer<StateType>], withMiddleware middlewares: [Middleware<StateType>]?)
 
     /// Dispatch an action through the reducers to mutate the state
     ///
@@ -48,15 +52,26 @@ public final class DefaultStore<StateType: State>: Store {
         }()
 
     let reducers: [Reducer<StateType>]
+	let middlewares: [Middleware<StateType>]
 
-    public init(withReducers reducers: [Reducer<StateType>]) {
+    public init(withReducers reducers: [Reducer<StateType>], withMiddleware middlewares: [Middleware<StateType>]? = nil) {
         self.reducers = reducers
+		if let middlewares = middlewares {
+			self.middlewares = middlewares.reversed()
+		} else {
+			self.middlewares = [Middleware<StateType>]()
+		}
     }
 
     public func dispatch<ActionType: Action> (action: ActionType) {
         // every received action is converted to an async action
         action
             .toAsync()
+			.do(onNext: { (action) in
+				self.middlewares.forEach({ (middleware) in
+					middleware(self.stateSubject.value, action)
+				})
+			})
             .map { [unowned self] (action) -> StateType? in
                 return self.reducers.reduce(self.stateSubject.value, { (currentState, reducer) -> StateType? in
                     return reducer(currentState, action)
