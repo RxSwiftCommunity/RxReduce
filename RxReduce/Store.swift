2f6d2bf6ba390b99fc86cf304731b3476dc8a02d
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2018-05-21 22:43:22 -0400

    Add the possibility to listen for a sub state

--- RxReduce/Store.swift
+++ RxReduce/Store.swift
@@ -21,16 +21,16 @@ public typealias Middleware<StateType: State> = (_ state: StateType?, _ action: 
 public protocol StoreType {
 
     /// A store is dedicated to the mutation/observation of this StateType
-    associatedtype StateType: State
+    associatedtype StateType: State & Equatable
 
-    /// The current State (UI compliant)
-    var state: Driver<StateType> { get }
+    /// The current State or SubState (UI compliant)
+    func state<SubStateType: Equatable> (from: @escaping (StateType) -> SubStateType) -> Driver<SubStateType>
 
     /// Inits the Store with its reducers stack
     ///
     /// - Parameter reducers: the reducers to be executed by the dispatch function
-	/// - Parameter middlewares: the middlewartes to be executed by the dispatch function (the will be executed in reverse order)
-	init(withReducers reducers: [Reducer<StateType>], withMiddlewares middlewares: [Middleware<StateType>]?)
+    /// - Parameter middlewares: the middlewartes to be executed by the dispatch function (the will be executed in reverse order)
+    init(withReducers reducers: [Reducer<StateType>], withMiddlewares middlewares: [Middleware<StateType>]?)
 
     /// Dispatch an action through the reducers to mutate the state
     ///
@@ -39,20 +39,24 @@ public protocol StoreType {
 }
 
 /// A default store that will handle a specific kind of State
-public final class Store<StateType: State>: StoreType {
+public final class Store<StateType: State & Equatable>: StoreType {
 
     private let disposeBag = DisposeBag()
 
     private let stateSubject = BehaviorRelay<StateType?>(value: nil)
-    public lazy var state: Driver<StateType> = { [unowned self] in
+    let reducers: [Reducer<StateType>]
+    let middlewares: [Middleware<StateType>]?
+
+    public func state<SubStateType: Equatable>(from: @escaping (StateType) -> SubStateType = { (state: StateType) in return (state as! SubStateType) }) -> Driver<SubStateType> {
         return self.stateSubject
             .asDriver()
             .filter { $0 != nil }
             .map { $0! }
-        }()
-
-    let reducers: [Reducer<StateType>]
-	let middlewares: [Middleware<StateType>]?
+            .map { (state) -> SubStateType in
+                return from(state)
+            }
+            .distinctUntilChanged()
+    }
 
     public init(withReducers reducers: [Reducer<StateType>], withMiddlewares middlewares: [Middleware<StateType>]? = nil) {
         self.reducers = reducers
@@ -63,11 +67,11 @@ public final class Store<StateType: State>: StoreType {
         // every received action is converted to an async action
         action
             .toAsync()
-			.do(onNext: { (action) in
-				self.middlewares?.forEach({ [unowned self] (middleware) in
-					middleware(self.stateSubject.value, action)
-				})
-			})
+            .do(onNext: { [unowned self] (action) in
+                self.middlewares?.forEach({ [unowned self] (middleware) in
+                    middleware(self.stateSubject.value, action)
+                })
+            })
             .map { [unowned self] (action) -> StateType? in
                 return self.reducers.reduce(self.stateSubject.value, { (currentState, reducer) -> StateType? in
                     return reducer(currentState, action)
